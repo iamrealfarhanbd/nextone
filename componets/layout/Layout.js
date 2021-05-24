7e9f5ff9157448249e2de971f43f56e65c76031a
unknown <iamfarhan09@gmail.com>
2021-05-25 01:08:37 +0600

    nextone

---
+++ componets/layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainHeader from './MainHeader'
+function Layout(props) {
+    return (
+        <>
+        <MainHeader />
+            <main>
+                {props.children}
+            </main>
+        </>
+    )
+}
+
+export default Layout
